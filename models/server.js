6c7132568c9b93aeb67f0367681d6832470b7479
robert <roberto.zarate@morphygps.com.mx>
2021-12-27 18:23:57 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        //http server
+        this.server = http.createServer(this.app);
+        
+        //Confuguraciones de sockets
+        
+        this.io = socketio(this.server, { /* configuraciones */});
+    }
+
+    middlewares(){
+        //Desplegar el directorio público
+        this.app.use(express.static(path.resolve( __dirname, '../public')));
+    }
+
+    configurarSockets(){
+        new Sockets(this.io);
+    }
+
+    execute(){
+        //inicializar Middlewares
+        this.middlewares();
+        //Inicializar sockets
+        this.configurarSockets();
+        //Inicializar server
+        this.server.listen(this.port, () =>{
+            console.log('Server corriendo en puerto:', this.port);
+        });
+    }
+}
+
+module.exports = Server;
